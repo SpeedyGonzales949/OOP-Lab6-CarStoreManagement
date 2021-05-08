d2ce411417379837830b9984bf5d115e91eed19e
Denisa Dragota <81757841+denisadragota@users.noreply.github.com>
2021-05-09 00:10:17 +0300

    Update App.cpp

--- OOP-Lab5/App.cpp
+++ OOP-Lab5/App.cpp
@@ -7,7 +7,7 @@ int main() {
 	shared_ptr<Repository::Garage> garage(new Repository::Garage("date.txt"));
 	ClientController::Client client = ClientController::Client(garage);
 	ManagerController::Manager manager = ManagerController::Manager(garage);
-	UI::Ui ui = UI::Ui(client,manager);
+	UI::Ui ui = UI::Ui(client, manager, "Login.txt");
 	ui.start();
 	std::cout << "Good Job!";
 	return 0;
